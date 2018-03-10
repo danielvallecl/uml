c68e76ece225fc3f241232f031d70e7d88e90be8
Daniel Valle <daniel.vallecl@gmail.com>
2018-03-10 17:55:29 -0500

    Created getters and setters

--- cursomc/src/main/java/com/danielvalle/cursomc/domain/Categoria.java
+++ cursomc/src/main/java/com/danielvalle/cursomc/domain/Categoria.java
@@ -1,5 +1,33 @@
 package com.danielvalle.cursomc.domain;
 
 public class Categoria {
+	
+	private Integer id;
+	private String nome;
 
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Categoria() {		//Empty Constructor
+		
+	}
+
+	public Integer getId() {	 	//Getter for Id
+		return id;
+	}
+
+	public void setId(Integer id) {		//Setter for Id
+		this.id = id;
+	}
+
+	public String getNome() {		//Getter for nome
+		return nome;
+	}
+
+	public void setNome(String nome) {		//Setter for nome
+		this.nome = nome;
+	}
 }
