4cba931253800eb986b4a1addcc5a28d1d523ae3
Daniel Valle <daniel.vallecl@gmail.com>
2018-03-11 17:22:26 -0400

    Stanford's Database Course Notes

--- cursomc/src/main/java/com/danielvalle/cursomc/domain/Categoria.java
+++ cursomc/src/main/java/com/danielvalle/cursomc/domain/Categoria.java
@@ -1,6 +1,10 @@
 package com.danielvalle.cursomc.domain;
 
-public class Categoria {
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String nome;
@@ -19,7 +23,7 @@ public class Categoria {
 		return id;
 	}
 
-	public void setId(Integer id) {		//Setter for Id
+	public void setId(Integer id) {		//Setter for Id. A setter is void because it doesn't return anything.
 		this.id = id;
 	}
 
@@ -30,4 +34,31 @@ public class Categoria {
 	public void setNome(String nome) {		//Setter for nome
 		this.nome = nome;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
